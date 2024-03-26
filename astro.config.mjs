1a17cc239dbe470fd0aaa602d27e8a22ec9a95d7
Nanda Kresnatara <nandayavanets@mail.ru>
2024-03-26 06:46:14 +0000

    add: keystatic config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [react(), markdoc()]
+});
