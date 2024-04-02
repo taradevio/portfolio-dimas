dbaaadc043a6d6ddc82f89eae22f2b75a0d32f28
Nanda Kresnatara <nandayavanets@mail.ru>
2024-04-02 15:15:43 +0000

    change speed

--- src/scripts/typewritter.js
+++ src/scripts/typewritter.js
@@ -2,7 +2,7 @@ import Typed from "typed.js";
 
       const autoType = new Typed(".auto-type", {
         strings: ["Dimas", "a Graphic Designer"],
-        typeSpeed: 200,
-        backSpeed: 200,
+        typeSpeed: 90,
+        backSpeed: 90,
         loop: true,
       });
