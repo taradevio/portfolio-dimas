e17f86ffd8439ea3c1cd4672868160179abdd172
Nanda Kresnatara <nandayavanets@mail.ru>
2024-04-04 14:18:26 +0000

    remove dist

--- dist/server/chunks/prerender_bi79ze8A.mjs
+++
@@ -1,8 +0,0 @@
-const noop = () => {};
-export const a = noop;
-export const c = noop;
-export const e = noop;
-export const i = noop;
-export const k = noop;
-export const p = noop;
-export const s = noop;
