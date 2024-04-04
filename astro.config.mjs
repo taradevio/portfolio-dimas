d0e0bbecec1909a16d84cc7ef5d4f3ad8998fee7
Nanda Kresnatara <nandayavanets@mail.ru>
2024-04-04 13:50:08 +0000

    remove dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,4 +13,7 @@ export default defineConfig({
   }),
   integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
+  build: {
+    assets: 'src/assets'
+  }
 });
--- dist/server/chunks/equilibrium_BrXVxqAZ.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_C6CHY5-D.mjs').then(n => n.e);
-
-export { page };
