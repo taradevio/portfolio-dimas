b6dad47d7c47db6fe1cf471c7a7a5ed43e377b90
Nanda Kresnatara <nandayavanets@mail.ru>
2024-04-04 13:54:38 +0000

    npm run build

---
+++ dist/server/chunks/eska_C5MW_0Sk.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_D2bFCvkI.mjs').then(n => n.a);
+
+export { page };
---
+++ dist/server/chunks/keystatic-astro-page_87yGn94g.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/keystatic-astro-page_DSE6lrOE.mjs');
+
+export { page };
